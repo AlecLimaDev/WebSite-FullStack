de0cb122c97e0e2714b286a316dd7c3d03bed573
Alec Lima <aleclimadev@gmail.com>
2023-10-12 12:01:20 -0300

    feat: database mysql

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
@@ -7,13 +6,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+require("dotenv").config({});
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: ,
-  database: "signup"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
 app.post("/signup", (req, res) => {
