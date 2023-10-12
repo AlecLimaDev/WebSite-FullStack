50d9143f527e9734ed854e0d08991ad5bfed93cb
Alec Lima <aleclimadev@gmail.com>
2023-10-12 12:00:05 -0300

    feat: signup validation

---
+++ src/screens/SignUp/SignUpValidation.module.tsx
@@ -0,0 +1,34 @@
+import { ErrorType, FormValues } from "../../types/SignUpValidation.module";
+
+function SignUpValidation(values: FormValues): ErrorType {
+    let errors: ErrorType = {};
+    const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/;
+  
+    if (!values.name) {
+      errors.name = "O campo nome está vazio";
+    } else {
+      errors.name = "";
+    }
+  
+    if (!values.email) {
+      errors.email = "Email should not be empty";
+    } else if (!email_pattern.test(values.email)) {
+      errors.email = "Esse formato de email é inválido";
+    } else {
+      errors.email = "";
+    }
+  
+    if (!values.password) {
+      errors.password = "O campo senha está vazio";
+    } else if (!password_pattern.test(values.password)) {
+      errors.password = "Esse formato de senha é inválido";
+    } else {
+      errors.password = "";
+    }
+  
+    return errors;
+  }
+  
+
+export default SignUpValidation;
