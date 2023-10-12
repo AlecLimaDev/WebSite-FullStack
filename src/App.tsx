221ff6443782ced63608aa86e5392c8adcf3f8f6
Alec Lima <aleclimadev@gmail.com>
2023-10-12 12:03:15 -0300

    feat: routes application

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./screens/Home/Home.module";
+import Login from "./screens/Login/Login.module";
+import SignUp from "./screens/SignUp/SignUp.module";
 
 function App() {
-
   return (
     <>
-      Hello
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
